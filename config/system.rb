a14038c326bfcff0bf1c6606481f0c79cd580fab
Andrew Rudenko <rudenkoco@gmail.com>
2010-02-05 17:55:08 +0300

    New paths

--- config/system.rb
+++ config/system.rb
@@ -2,8 +2,8 @@ GoodGem.config.merge!({
   :platforms => ['ubuntu', 'debian'],
   :archs => ['i386', 'amd64'],
   :main_maintainer => { :name => 'Andrew Rudenko', :email => 'ceo@prepor.ru'},
-  :pool_path => Pathname.new(File.join(RAILS_ROOT, 'public/pool')),
-  :dists_path => Pathname.new(File.join(RAILS_ROOT, 'public/dists')),
+  :pool_path => Pathname.new('/home/gg/shared/pool'),
+  :dists_path => Pathname.new('/home/gg/shared/dists'),
   :specs_url => 'http://gemcutter.org/specs.4.8.gz',
   :gems_api_url => 'http://gemcutter.org/api/v1/gems/'
 })
